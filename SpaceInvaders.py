3e9a67119e61b0176d98cc01eec60da0b2edea58
ZainLatif7164 <latifzain40@gmail.com>
2023-12-01 03:00:57 +0100

    Ufo's X and Y movement added bullets added kinda

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # pygame setup
 
@@ -12,6 +13,9 @@ running = True
 spaceship_img = pygame.image.load("spaceship.png")
 spaceship_img = pygame.transform.scale(spaceship_img, (50, 60))
 bg_img = pygame.image.load("spaceinvadersBG.png")
+alien_png=pygame.image.load("ufo.png")
+
+    
 
 # Screen Name and Space Ship
 
@@ -23,9 +27,22 @@ x_left = 30
 y_top = 450
 y_bottom = 720
 
+#Alien Poisitions
+
+alien_x=random.randint(30,350)
+alien_y=random.randint(30,100)
+
+# Alien going to x side and y side
+
+alien_x1=random.uniform(-0.4 , 0.4)
+alien_y1=random.uniform(0.2 , 1)
+
+# 
+
 # Key Press Speed (Number of pixels space ship moves on each press)
 
 press_speed=5
+bullets_img= pygame.image.load("bullets.png")
 
 # Space Ship position 
 
@@ -49,11 +66,21 @@ while running:
         player_pos.x -= press_speed
     if keys[pygame.K_RIGHT] and player_pos.x < x_right:
         player_pos.x += press_speed
-
-    screen.blit(bg_img, (0,0))
+    # if keys[pygame.K_SPACE]:
+        
+        
+        
+    if alien_x < x_right and alien_x >x_left: 
+        alien_x += alien_x1
+    alien_y += alien_y1
+    
+    # screen.blit(bg_img, (0,0))
+    screen.fill("white")
     screen.blit(spaceship_img, (player_pos.x, player_pos.y))
+    # screen.blit(bullets_img, (player_pos.x, player_pos.y))
+    screen.blit(alien_png,(alien_x ,alien_y))
     
     pygame.display.flip()
     clock.tick(60)
-
+    
 pygame.quit()
